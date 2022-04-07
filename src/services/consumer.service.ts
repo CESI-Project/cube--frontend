1e95cc23f5e7db5b6a86b8eefc16d6a361ad98d8
Gadiffet <theo.gadiffet@gmail.com>
2022-04-07 22:54:59 +0200

    fix: delete useless file

--- src/services/consumer.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
-
-const getConsumer = () => useQuery('consumer', () => axios.get('http://localhost:8080/api/v1/consumer').then((response) => response.data));
-
-export default getConsumer;
