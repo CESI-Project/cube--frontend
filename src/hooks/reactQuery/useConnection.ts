90f6b858109e676ad80fdee06b09ee666382e4ee
Gadiffet <theo.gadiffet@gmail.com>
2022-04-26 21:53:15 +0200

    feat(werning): add warning

--- src/hooks/reactQuery/useConnection.ts
+++ src/hooks/reactQuery/useConnection.ts
@@ -2,7 +2,7 @@ import { useMutation } from 'react-query';
 import { postConnection } from '../../services/consumer.service';
 
 export const useConnection = () => {
-  const { mutate } = useMutation('connection', postConnection);
+  const { mutate, isError } = useMutation('connection', postConnection);
 
-  return { mutate };
+  return { mutate, isError };
 };
--- src/pages/connectionPage/ConnectionPage.component.tsx
+++ src/pages/connectionPage/ConnectionPage.component.tsx
@@ -1,45 +1,44 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import './ConnectionPage.component.scss';
 import { InputComponent } from '../../components/input/input.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { useConnection } from '../../hooks/reactQuery/useConnection';
-import { Consumer } from '../../models/Consumer';
 
 const messages = defineMessages({
-  ConnectionPage_mailInput: {
+  connectionPage_mailInput: {
     defaultMessage: 'Your email',
     id: 'connectionPage.mailInput',
   },
-  ConnectionPage_passwordInput: {
+  connectionPage_passwordInput: {
     defaultMessage: 'Your password',
     id: 'connectionPage.passwordInput',
   },
-  ConnectionPage_connectionTitle: {
+  connectionPage_connectionTitle: {
     defaultMessage: 'Log In',
     id: 'connectionPage.connectionTitle',
   },
-  ConnectionPage_mailTitle: {
+  connectionPage_mailTitle: {
     defaultMessage: 'Mail :',
     id: 'connectionPage.mailTitle',
   },
-  ConnectionPage_passwordTitle: {
+  connectionPage_passwordTitle: {
     defaultMessage: 'Password :',
     id: 'connectionPage.passwordTitle',
   },
-  ConnectionPage_submitConnection: {
+  connectionPage_submitConnection: {
     defaultMessage: 'Log In',
     id: 'connectionPage.submitConnection',
   },
+  connectionPage_errorInput: {
+    defaultMessage: 'Your Mail / Password is wrong',
+    id: 'connectionPage.errorInput',
+  },
 });
 
-interface ConnectionPageComponentProps {
-  truc: Consumer
-}
-
-export const ConnectionPageComponent: FC<ConnectionPageComponentProps> = () => {
+export const ConnectionPageComponent = () => {
   const { formatMessage } = useIntl();
-  const { mutate } = useConnection();
+  const { mutate, isError } = useConnection();
 
   const onConnection = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -51,21 +50,26 @@ export const ConnectionPageComponent: FC<ConnectionPageComponentProps> = () => {
 
   return (
     <div className="connectionPage">
-      <h1>{formatMessage(messages.ConnectionPage_connectionTitle)}</h1>
+      <h1>{formatMessage(messages.connectionPage_connectionTitle)}</h1>
       <form className="connectionPage__wrapper" onSubmit={onConnection}>
+        {isError && (
+        <div className="connectionPage__error-input">
+          {formatMessage(messages.connectionPage_errorInput)}
+        </div>
+        )}
         <div>
           <h2>
-            {formatMessage(messages.ConnectionPage_mailTitle)}
+            {formatMessage(messages.connectionPage_mailTitle)}
           </h2>
-          <InputComponent name="email" type="email" inputsize="small" placeholder={formatMessage(messages.ConnectionPage_mailInput)} />
+          <InputComponent name="email" type="email" inputsize="small" placeholder={formatMessage(messages.connectionPage_mailInput)} />
         </div>
         <div>
-          <h2>{formatMessage(messages.ConnectionPage_passwordTitle)}</h2>
-          <InputComponent name="password" type="password" inputsize="small" placeholder={formatMessage(messages.ConnectionPage_passwordInput)} />
+          <h2>{formatMessage(messages.connectionPage_passwordTitle)}</h2>
+          <InputComponent name="password" type="password" inputsize="small" placeholder={formatMessage(messages.connectionPage_passwordInput)} />
         </div>
         <div className="connectionPage__wrapper__connection-button">
           <ButtonComponent type="submit" designType="full">
-            {formatMessage(messages.ConnectionPage_submitConnection)}
+            {formatMessage(messages.connectionPage_submitConnection)}
           </ButtonComponent>
         </div>
       </form>
