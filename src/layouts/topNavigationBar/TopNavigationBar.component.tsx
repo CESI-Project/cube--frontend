d7af2a357c2630b61b7a7595503b8d15534f1d5c
Sebpostiaux <75486525+Sebpostiaux@users.noreply.github.com>
2022-05-01 22:07:47 +0200

    feat(responsiv): add responsiv topnav

--- src/layouts/topNavigationBar/TopNavigationBar.component.tsx
+++ src/layouts/topNavigationBar/TopNavigationBar.component.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { defineMessages, useIntl } from 'react-intl';
 import './TopNavigationBar.component.scss';
+import { useState } from 'react';
+import { InputComponent } from '../../components/input/input.component';
 
 const messages = defineMessages(
   {
@@ -24,32 +26,94 @@ const messages = defineMessages(
       defaultMessage: 'Settings',
       id: 'topNavBar.Settings',
     },
+    topNavBar_searchBar: {
+      defaultMessage: 'Search',
+      id: 'topNavBar.searchBar',
+    },
+    topNavBar_News: {
+      defaultMessage: 'News',
+      id: 'topNavBar.News',
+    },
+    topNavBar_Messages: {
+      defaultMessage: 'Messages',
+      id: 'topNavBar.Messages',
+    },
   },
 );
 
 export const TopNavigationBarComponent = () => {
   const { formatMessage } = useIntl();
-
+  const [isLabel, setIsLabel] = useState(false);
+  const onLabel = () => { setIsLabel(!isLabel); };
   return (
     <nav className="topNavBar">
-      <div className="topNavBar__left">
-        <Link to={formatMessage(messages.topNavBar_Home).toLowerCase()}>
-          {formatMessage(messages.topNavBar_Home)}
-        </Link>
-        <Link to={formatMessage(messages.topNavBar_Categories).toLowerCase()}>
-          {formatMessage(messages.topNavBar_Categories)}
-        </Link>
-        <Link to={formatMessage(messages.topNavBar_Favorites).toLowerCase()}>
-          {formatMessage(messages.topNavBar_Favorites)}
-        </Link>
+        <div className="topNavBarResponsive">
+        <button className="buttonResponsive" onClick={onLabel}>
+          ☰
+        </button>
+        {isLabel && (
+        <div className="divNavBarResponsive">
+          <ul>
+            <li>
+              <Link to={formatMessage(messages.topNavBar_Home).toLowerCase()}>
+                {formatMessage(messages.topNavBar_Home)}
+              </Link>
+            </li>
+            <li>
+              <Link to={formatMessage(messages.topNavBar_News).toLowerCase()}>
+                {formatMessage(messages.topNavBar_News)}
+              </Link>
+            </li>
+            <li>
+              <Link to={formatMessage(messages.topNavBar_Messages).toLowerCase()}>
+                {formatMessage(messages.topNavBar_Messages)}
+              </Link>
+            </li>
+            <li>
+              <Link to={formatMessage(messages.topNavBar_CreateTopic).toLowerCase()}>
+                {formatMessage(messages.topNavBar_CreateTopic)}
+              </Link>
+            </li>
+            <li>
+              <Link to={formatMessage(messages.topNavBar_Settings).toLowerCase()}>
+                {formatMessage(messages.topNavBar_Settings)}
+              </Link>
+            </li>
+          </ul>
+
+        </div>
+
+        )}
+        <InputComponent name="search-bar" type="search" placeholder={formatMessage(messages.topNavBar_searchBar)} inputsize="small" />
+
       </div>
-      <div className="topNavBar__right">
-        <Link to={formatMessage(messages.topNavBar_CreateTopic).toLowerCase()}>
-          {formatMessage(messages.topNavBar_CreateTopic)}
-        </Link>
-        <Link to={formatMessage(messages.topNavBar_Settings).toLowerCase()}>
-          {formatMessage(messages.topNavBar_Settings)}
-        </Link>
+      {/* <div className="responsive"> */}
+      {/* <div className="test"> */}
+      {/*   <label htmlFor="toggle" className="topNavBar__label">☰</label> */}
+      {/*   <input type="checkbox" id="toggle" /> */}
+      {/* </div> */}
+
+      {/* </div> */}
+      <div className="divNavBar">
+        <div className="topNavBar__left">
+          <Link to={formatMessage(messages.topNavBar_Home).toLowerCase()}>
+            {formatMessage(messages.topNavBar_Home)}
+          </Link>
+          <Link to={formatMessage(messages.topNavBar_News).toLowerCase()}>
+            {formatMessage(messages.topNavBar_News)}
+          </Link>
+          <Link to={formatMessage(messages.topNavBar_Messages).toLowerCase()}>
+            {formatMessage(messages.topNavBar_Messages)}
+          </Link>
+        </div>
+        <div className="topNavBar__right">
+          <Link to={formatMessage(messages.topNavBar_CreateTopic).toLowerCase()}>
+            {formatMessage(messages.topNavBar_CreateTopic)}
+          </Link>
+          <Link to={formatMessage(messages.topNavBar_Settings).toLowerCase()}>
+            {formatMessage(messages.topNavBar_Settings)}
+          </Link>
+        </div>
       </div>
     </nav>
   );
