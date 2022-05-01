5abaecf2b1f57103091383f04400ee295befd827
Gadiffet <theo.gadiffet@gmail.com>
2022-05-01 23:27:24 +0200

    fix(model): delete useless value

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,13 @@
+import { Comment } from './Comment';
+
 export interface User {
     id?: number;
     userName: string;
     birthDate: string;
     email: string;
     password: string;
+    comment?: Comment[];
     age?: number;
+    favorite?: string[];
     role?: string[];
 }
--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -6,7 +6,7 @@ import { authHeader } from '../context/authHeader';
 export const getAllComments = async (id: number) => axios.get(`${BACKEND_URI}/comment/${id}`).then((response) => response.data);
 
 export const postCreationComment = async ({
-  text, userId, topicId, react, dislike, createdAt,
+  text, userId, topicId, createdAt,
 }: Comment) => axios.post(`${BACKEND_URI}/comment`, {
-  text, userId, topicId, react, dislike, createdAt,
+  text, userId, topicId, createdAt,
 }, { headers: authHeader() }).then((response) => response.data);
