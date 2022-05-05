9b4a8318c1237bd40a9773b00261ba43ce8aba0c
System Administrator <valentin.lacour@viacesi.fr>
2022-05-05 10:27:56 +0200

    feat(sub comment): add button delete

--- src/pages/topicPage/commentZone/subCommentZone/SubCommentZone.container.tsx
+++ src/pages/topicPage/commentZone/subCommentZone/SubCommentZone.container.tsx
@@ -1,15 +1,27 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { SubCommentZoneComponent } from './SubCommentZone.component';
 import { User } from '../../../../models/User';
+import { deleteSubComment } from '../../../../services/subcomment.service';
+import { useSubCommentsByComment } from '../../../../hooks/reactQuery/useSubCommentsByComment';
 
 interface SubCommentZoneContainerProps {
   commentId: number | undefined;
   currentUser: User | undefined;
 }
 
-export const SubCommentZoneContainer: FC<SubCommentZoneContainerProps> = ({ commentId, currentUser }) => {
-  const a = () => {
 
+export const SubCommentZoneContainer: FC<SubCommentZoneContainerProps> = ({ commentId, currentUser }) => {
+  const { subComments, refetch } = useSubCommentsByComment(commentId);
+  const [showComment, isShowComment] = useState<boolean>(false);
+
+  const onShowComment = () => {
+    refetch();
+    isShowComment(!showComment);
   };
-  return (<SubCommentZoneComponent commentId={commentId} currentUser={currentUser} />);
+
+  const onDeleteSubComment = (id: number | undefined) => {
+    deleteSubComment(id);
+    setTimeout(refetch, 100);
+  };
+  return (<SubCommentZoneComponent onShowComment={onShowComment} subComments={subComments} showComment={showComment} onDeleteSubComment={onDeleteSubComment} commentId={commentId} currentUser={currentUser} />);
 };
--- src/services/subcomment.service.ts
+++ src/services/subcomment.service.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { BACKEND_URI } from './index';
 import { SubComment } from '../models/SubComment';
+import { authHeader } from '../context/authHeader';
 
-export const getSubCommentByComment = async (id: number | undefined) => axios.get(`${BACKEND_URI}/responseComment/${id}`).then((response) => response.data);
+export const getSubCommentByComment = async (id: number | undefined) => axios.get(`${BACKEND_URI}/responseComment/${id}`, { headers: authHeader() }).then((response) => response.data);
 
 export const postCreationSubComment = async ({
   text, createdAt, commentId, userId,
 }: SubComment) => axios.post(`${BACKEND_URI}/responseComment/`, {
   text, createdAt, commentId, userId,
 }).then((response) => response.data);
+
+export const deleteSubComment = async (id: number | undefined) => axios.delete(`${BACKEND_URI}/responseComment/${id}`, { headers: authHeader() }).then((response) => response.data);
